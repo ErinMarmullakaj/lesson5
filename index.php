06d7851891deba6c48714456de8738736f15696f
ErinMarmullakaj <erin.marmullakaj2@gmail.com>
2024-10-01 17:31:25 +0200

    ..

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// phpinfo();
+
+
+// $x = "hello";
+// print_r($x);
+
+// $x= 345;
+// echo gettype($x). "<br>";
+// $y = 9.7;
+// echo gettype($y). "<br>";
+// $z = "hello"."mirdita";
+// echo gettype($z). "<br>";
+
+// function displayPHPVersion(){
+//     echo "this is php ".phpversion();
+//     echo "\n";
+// }
+
+// displayPHPVersion();
+
+// function sum(){
+//     value = 546+876;
+//     echo $value;
+// }
+
+// sum();
+
+
+
+
+
+
+
+
+?>
